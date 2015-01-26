9228b419c29a4ed1f001317dc1fa6e15588d0b1a
Yamila Moreno <yamila.ms@gmail.com>
2015-01-26 09:58:00 +0100

    Initial commit

---
+++ src/test/groovy/LibraryTest.groovy
@@ -0,0 +1,13 @@
+import spock.lang.Specification
+
+class LibraryTest extends Specification{
+    def "someLibraryMethod returns true"() {
+        setup:
+        Library lib = new Library()
+        println "***************************************"
+        when:
+        def result = lib.someLibraryMethod()
+        then:
+        result == true
+    }
+}
